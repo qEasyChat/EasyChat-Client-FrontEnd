ae1953a3c31a2c0003f43c441fe381769c2aa3ec
q1e123 <robertnicolaesolca@gmail.com>
2022-02-22 09:25:48 +0200

    added safe append text

---
+++ SafeGUI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace EasyChat_Client_FrontEnd
+{
+    class SafeGUI
+    {
+        public static void safeAppendText(TextBox textBox, string str)
+        {
+            if (textBox.Dispatcher.CheckAccess())
+            {
+                textBox.AppendText(str);
+                textBox.AppendText(Environment.NewLine);
+                textBox.ScrollToEnd();
+            }
+            else textBox.Dispatcher.Invoke(new Action<TextBox, string>(safeAppendText), textBox, str);
+        }
+
+    }
+}
